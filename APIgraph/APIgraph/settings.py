1634456756173a6fcd1847a8b69e5013f9b79599
Fernando Heredia <71789047+nanocarp01@users.noreply.github.com>
2021-09-24 10:57:24 -0300

    Update settings.py

--- APIgraph/APIgraph/settings.py
+++ APIgraph/APIgraph/settings.py
@@ -82,9 +82,9 @@ WSGI_APPLICATION = 'APIgraph.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'api_prueba',
+        'NAME': 'xxxxxx',
         'USER': 'postgres',
-        'PASSWORD': 'A1234',
+        'PASSWORD': 'xxxxx',
         'HOST': 'localhost',
         'PORT': '',
     }
@@ -146,5 +146,5 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'pruebatest0103@gmail.com'
-EMAIL_HOST_PASSWORD = 'apzLXzjBKYnHXA4'
+EMAIL_HOST_USER = 'xxxxxxxxxxxxxxx'
+EMAIL_HOST_PASSWORD = 'xxxxxxxxx'
